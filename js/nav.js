a11986c74653299539fccb4014f437033adeff35
root <root@Punit-BeastPC.localdomain>
2021-03-04 10:29:41 +1100

    Fix 'overflow-x' issue, add menu animation and functionality

--- js/nav.js
+++ js/nav.js
@@ -1,13 +1,11 @@
-const navSlide = () => {
-const burger = document.querySelector('.nav-burger');
-const nav = document.querySelector('.nav-links');
-const navLinks = document.querySelectorAll('.nav-links li');
-    //Toggle Nav
+function showNav()
+{
+    var button  = document.querySelector('.nav-button');
+    var nav     = document.querySelector('.nav-links');
+    var navLinks= document.querySelectorAll('.nav-links li');
 
-    burger.addEventListener('click', () => {
-      nav.classList.toggle('nav-active');
+    nav.classList.toggle('nav-active');
 
-    //Animate Links
     navLinks.forEach((link, index) => {
       if (link.style.animation){
         link.style.animation = ''
@@ -15,13 +13,4 @@ const navLinks = document.querySelectorAll('.nav-links li');
         link.style.animation = `navLinkFade 0.15s ease forwards ${index / 9 + 1}s`;
       }
     });
-  });
 }
-navSlide();
-
-// function showNav()
-// {
-//     var links = document.querySelector('.nav-links');
-//     links.classList.toggle('nav-active');
-// }
-

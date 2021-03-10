c5af808953f6329e4378d895a4fca2817de3d1a7
PunitDh <cas012115@coderacademy.edu.au>
2021-03-10 12:08:30 +1100

    Fix CSS image issue for index, portfolio and about pages

---
+++ js/fortune.js
@@ -0,0 +1,23 @@
+function httpGet(theUrl)
+{
+    let xmlhttp;
+    
+    if (window.XMLHttpRequest) { // code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp=new XMLHttpRequest();
+    } else { // code for IE6, IE5
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    
+    xmlhttp.onreadystatechange=function() {
+        if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+            return xmlhttp.responseText;
+        }
+    }
+    xmlhttp.open("GET", theUrl, false);
+    xmlhttp.send();
+    
+    return xmlhttp.response;
+}
+
+
+document.getElementById("fortune").innerHTML = httpGet("http://fortuneapi.herokuapp.com/");

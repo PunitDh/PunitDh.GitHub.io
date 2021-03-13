5b0e1534d9cd8c04e9b327d6598a068030dd84ec
root <root@Punit-BeastPC.localdomain>
2021-03-14 08:13:16 +1100

    Fix media breakpoints in CSS for all pages

--- js/nav.js
+++ js/nav.js
@@ -1,3 +1,11 @@
+/*
+ * Filename:    Nav.JS (http://punitdh.github.io/js/nav.js)
+ * Author:      Punit Dharmadhikari
+ * Github:      https://github.com/PunitDh
+ * Description: This is a JavaScript file used for controlling the navigation 
+ *              bar for the website found at http://punitdh.github.io/
+ */
+
 function showNav()
 {
     var navMenu   = document.querySelector('.nav-links');

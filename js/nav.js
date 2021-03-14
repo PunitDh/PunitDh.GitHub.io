e5a056fb7455bdc82ff82e7a86e95a225d241e18
root <root@Punit-BeastPC.localdomain>
2021-03-14 13:49:07 +1100

    Update About Me page, update ReadMe.MD file

--- js/nav.js
+++ js/nav.js
@@ -8,19 +8,17 @@
 
 function showNav()
 {
-    var navMenu   = document.querySelector('.nav-links');
+    var navMenu = document.querySelector('.nav-links');
     var navButton = document.querySelector('.nav-button');
     var stickyBar = document.querySelector('.sticky-bar');
-    var navLink   = document.querySelectorAll('.nav-links li');
+    var navLink = document.querySelectorAll('.nav-links li');
 
     navMenu.classList.toggle('nav-active');
     navButton.classList.toggle('nav-button-active');
     stickyBar.classList.toggle('sticky-bar-active');
 
-    navLink.forEach((link, index) => 
-    {
-        with (link.style)
-        {
+    navLink.forEach((link, index) => {
+        with (link.style) {
             if (animation) animation = ''
             else animation = 'navBarAnimation 0.2s ease forwards';
         }
